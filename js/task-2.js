2d71294949900566002b9df3460114460743e6a2
Dmitriy Dihtiarov <dihtiarovd@gmail.com>
2021-02-14 16:30:06 +0200

    finish task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,30 @@
-const countProps = function (obj) {
-  
-  const arr = Object.keys(obj);
-  return arr.length;
+class User{
 
-};
+  constructor({ name, age, followers }) {
+    this.name = name;
+    this.age = age;
+    this.followers = followers;
+  }
 
-console.log(countProps({})); // 0
+  getInfo() {
+    console.log(`User ${this.name} is ${this.age} years old and has ${this.followers}
+  followers`);
+  }
+}
 
-console.log(countProps({ name: 'Mango', age: 2 })); // 2
 
-console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+const mango = new User({
+  name: 'Mango',
+  age: 2,
+  followers: 20,
+});
+
+mango.getInfo(); // User Mango is 2 years old and has 20 followers
+
+const poly = new User({
+  name: 'Poly',
+  age: 3,
+  followers: 17,
+});
+
+poly.getInfo(); // User Poly is 3 years old and has 17 followers
